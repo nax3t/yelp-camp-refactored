fccc159504ecc5db670afd5ec20d6dcaa1feb68a
Ian Schoonover <iantskon@gmail.com>
2017-03-01 15:23:44 -0600

    Initial commit

---
+++ models/campground.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   createdAt: { type: Date, default: Date.now },
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
---
+++ models/comment.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var commentSchema = mongoose.Schema({
+    text: String,
+    createdAt: { type: Date, default: Date.now },
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
