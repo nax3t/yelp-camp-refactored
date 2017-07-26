f66d9b3133f2dfd5f03cbb5e2f19dc5fd7a58466
Ian Schoonover <iantskon@gmail.com>
2017-07-25 21:59:07 -0500

    Update admin role code

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get("/register", function(req, res){
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
-    if(req.body.adminCode === 'secretcode123') {
+    if(req.body.adminCode === process.env.ADMIN_CODE) {
       newUser.isAdmin = true;
     }
     User.register(newUser, req.body.password, function(err, user){
