8c81126699fdf42fa32b25bbed452b14a88330fd
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2018-01-17 22:17:16 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -67,7 +67,7 @@ router.get("/users/:id", function(req, res) {
   User.findById(req.params.id, function(err, foundUser) {
     if(err) {
       req.flash("error", "Something went wrong.");
-      res.redirect("/");
+      return res.redirect("/");
     }
     Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
       if(err) {
